eb7acfa4ed4cce61c1bf182be4db4461c27751c2
heyhello <leshokpavel@gmail.com>
2018-10-25 20:59:15 +0300

    commit2

---
+++ ECMAScript_5/script.js
@@ -0,0 +1,14 @@
+
+function Bill(number, pin, balance, createDate, userName, userSurname, passportID ){
+ this.number = number;
+ this.pin = pin;
+ this.balance = balance;
+ this.createDate = createDate;
+ this.userName = userName;
+ this.userSurname = userSurname;
+ this.passportID = passportID;
+
+}
+
+
+
