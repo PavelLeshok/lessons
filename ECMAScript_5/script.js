6d427305a8f684b96b1ccd2dff4aac7d64e9e859
heyhello <leshokpavel@gmail.com>
2018-10-25 22:30:11 +0300

    commit3 c

--- ECMAScript_5/script.js
+++ ECMAScript_5/script.js
@@ -1,14 +1,48 @@
 
+document.body.onload = function(){
+    if( document.getElementById('billType').value === 'one' ){
+    document.getElementById('percentValue').setAttribute('disabled', 'true');
+    document.getElementById('withdrawalDate').setAttribute('disabled', 'true');
+    }
+}
+        
+
+
+
 function Bill(number, pin, balance, createDate, userName, userSurname, passportID ){
  this.number = number;
  this.pin = pin;
- this.balance = balance;
  this.createDate = createDate;
  this.userName = userName;
  this.userSurname = userSurname;
  this.passportID = passportID;
+ this.balance = balance;
 
 }
 
 
+
+
+
+
+
+
+
+
+
+document.getElementById('billType').onchange = function(){
+   if( document.getElementById('billType').value === 'one' ){
+    document.getElementById('withdrawalDate').setAttribute('disabled', 'true')
+       
+
+     
+   
+  
+       
+   } else if (document.getElementById('billType').value === 'two'){
+       console.log('two')
+   }
+    
+
+}
 
