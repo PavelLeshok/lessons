419e1ca929d18fa8f3be237220a0201046d15f60
heyhello <leshokpavel@gmail.com>
2018-10-26 12:32:20 +0300

    yahoo

--- ECMAScript_5/script.js
+++ ECMAScript_5/script.js
@@ -1,27 +1,31 @@
 
-    document.body.onload = function(){
+document.body.onload = function(){
     if( document.getElementById('billType').value === 'zero' ){
-    document.getElementById('cardType').setAttribute('disabled', 'true');
-    document.getElementById('currencyType').setAttribute('disabled', 'true');
-    document.getElementById('percentValue').setAttribute('disabled', 'true');
-    document.getElementById('withdrawalDate').setAttribute('disabled', 'true');
+        document.getElementById('cardType').setAttribute('disabled', 'true');
+        document.getElementById('currencyType').setAttribute('disabled', 'true');
+        document.getElementById('percentValue').setAttribute('disabled', 'true');
+        document.getElementById('withdrawalDate').setAttribute('disabled', 'true');
     }
 }
-    function change(){
-        if( document.getElementById('billType').value === 'one' ){
-            document.getElementById('percentValue').setAttribute('disabled', 'true');
-            document.getElementById('withdrawalDate').setAttribute('disabled', 'true');
-            document.getElementById('cardType').removeAttribute('disabled');
-            document.getElementById('currencyType').removeAttribute('disabled');  
-        } else if (document.getElementById('billType').value === 'two'){
-            document.getElementById('percentValue').removeAttribute('disabled');
-            document.getElementById('withdrawalDate').removeAttribute('disabled');
-            document.getElementById('cardType').setAttribute('disabled', 'true');
-            document.getElementById('currencyType').setAttribute('disabled', 'true');
-        }    
-     }
-    
+function change(){
+    if( document.getElementById('billType').value === 'one' ){
+        document.getElementById('percentValue').setAttribute('disabled', 'true');
+        document.getElementById('withdrawalDate').setAttribute('disabled', 'true');
+        document.getElementById('cardType').removeAttribute('disabled');
+        document.getElementById('currencyType').removeAttribute('disabled');  
+    }else if (document.getElementById('billType').value === 'two'){
+        document.getElementById('percentValue').removeAttribute('disabled');
+        document.getElementById('withdrawalDate').removeAttribute('disabled');
+        document.getElementById('cardType').setAttribute('disabled', 'true');
+        document.getElementById('currencyType').setAttribute('disabled', 'true');
+    }else if(document.getElementById('billType').value === 'zero'){
+        document.getElementById('cardType').setAttribute('disabled', 'true');
+        document.getElementById('currencyType').setAttribute('disabled', 'true');
+        document.getElementById('percentValue').setAttribute('disabled', 'true');
+        document.getElementById('withdrawalDate').setAttribute('disabled', 'true');
+}
 
+}
 document.getElementById('billType').addEventListener("change", change)
 
 
