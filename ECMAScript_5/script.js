801a1fd0a9fbe029970139309f74e6157b6f0edc
heyhello <leshokpavel@gmail.com>
2018-11-01 20:07:03 +0300

    yoyoyoyo

--- ECMAScript_5/script.js
+++ ECMAScript_5/script.js
@@ -1,41 +1,70 @@
 
 
 
-
-
-document.body.onload = function(){
-    if( document.getElementById('billType').value === 'zero' ){
-        document.getElementById('cardType').setAttribute('disabled', 'true');
-        document.getElementById('currencyType').setAttribute('disabled', 'true');
-        document.getElementById('percentValue').setAttribute('disabled', 'true');
-        document.getElementById('depositTerm').setAttribute('disabled', 'true');
-    }
-}
 function change(){
-    if( document.getElementById('billType').value === 'one' ){
-        document.getElementById('percentValue').setAttribute('disabled', 'true');
-        document.getElementById('depositTerm').setAttribute('disabled', 'true');
-        document.getElementById('cardType').removeAttribute('disabled');
-        document.getElementById('currencyType').removeAttribute('disabled');  
-    }else if (document.getElementById('billType').value === 'two'){
-        document.getElementById('percentValue').removeAttribute('disabled');
-        document.getElementById('depositTerm').removeAttribute('disabled');
-        document.getElementById('cardType').setAttribute('disabled', 'true');
-        document.getElementById('currencyType').setAttribute('disabled', 'true');
-    }else if(document.getElementById('billType').value === 'zero'){
-        document.getElementById('cardType').setAttribute('disabled', 'true');
-        document.getElementById('currencyType').setAttribute('disabled', 'true');
-        document.getElementById('percentValue').setAttribute('disabled', 'true');
-        document.getElementById('depositTerm').setAttribute('disabled', 'true');
-}
+    let html;
+    if( document.getElementById('billType').value === 'zero' ){
+        html = ``;
+    } else if (document.getElementById('billType').value === 'one'){
+        html = ` <div class="menu">
+        <span>Тип карты</span>
+        <select name="cardType" id="cardType">
+            <option value="one" selected>Visa</option>
+            <option value="two">Master Card</option>
+            <option value="three">Пуховичи Card</option>
+        </select>
+    </div>
+    <div class="menu" >
+        <span>Валюта</span>
+        <select name="currencyType" id="currencyType">
+            <option value="zero" selected>Выберите тип валюты</option>
+            <option value="one" >USD</option>
+            <option value="two">Euro</option>
+            <option value="three">Рупии</option>
+            <option value="four">Бундесшилинги</option>
+        </select>
+    </div>`
+    } else if(document.getElementById('billType').value === 'two'){
+        html = `  <div class="menu" > 
+        <span>Процент</span> 
+        <select name="percentValue" id="percentValue"> 
+            <option value="zero" selected>Выберите размер процента</option> 
+            <option value="one">1 %</option> 
+            <option value="two">2%</option> 
+            <option value="three">5%</option> 
+            <option value="four">10%</option> 
+        </select> 
+    </div>
+    <div class="menu" > 
+        <span>Срок вклада</span> 
+        <select name="depositTerm" id="depositTerm">
+            <option value="zero" selected disabled>Выберите срок вклада</option> 
+            <option value="one">Месяц</option> 
+            <option value="two">Пол года</option> 
+            <option value="three">Год</option> 
+            <option value="four">Пять лет</option> 
+        </select> 
+    </div>`
+    }
+    document.querySelector('.changer').innerHTML = html;  
+
 
 }
-document.getElementById('billType').addEventListener("change", change)
 
 
 
 
-        
+
+
+
+
+
+
+
+document.getElementById('billType').addEventListener('change', change);
+
+
+    
 
 
 
